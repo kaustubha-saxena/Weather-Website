e7a06dca52eccc0e6d55873cbc80f1e154ba442f
kaustubha-saxena <75664233+kaustubha-saxena@users.noreply.github.com>
2024-07-01 00:55:09 +0530

    completed

--- script.js
+++ script.js
@@ -1,6 +1,4 @@
 const api = "5195e647002e4f456ddedfbd1b54eb22";
-// const url="https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={API key}"
-// const url2="http://api.openweathermap.org/geo/1.0/direct?q=London&limit=5&appid={API key}";
 let temp = document.body.querySelector(".temp");
 let img = document.body.querySelector(".weather-img");
 let weather = document.body.querySelector(".weather");
@@ -8,6 +6,7 @@ let speed = document.body.querySelector(".speed")
 let search = document.body.querySelector(".search");
 let btn = document.body.querySelector(".button");
 let humidity = document.body.querySelector(".humidity_value");
+let reminder = document.body.querySelector(".tips")
 let city = "";
 let data;
 btn.addEventListener("click", () => {
@@ -24,6 +23,14 @@ function get_Data(lat, lon) {
             speed.innerHTML = Math.round(data.wind.speed * 3.6) + "Km/h";
             console.log(data.main.humidity)
             humidity.innerHTML = data.main.humidity + "%";
+            if (data.main.humidity >= 85) {
+                reminder.classList.remove("hide")
+                reminder.children[1].innerHTML = `Don't forget your umbrella! Humidity at ${data.main.humidity}% with high chance of rain today. Stay dry!`
+            }
+            else {
+                reminder.classList.add("hide")
+
+            }
         })
     })
 }
@@ -38,3 +45,5 @@ function get_city() {
         })
     })
 }
+search.children[0].value = "Delhi";
+get_city();
